862c46a4aa39235dcdf5d49e6e5b834b2b5ab120
Tatyana Shindollar <tshindollar@gmail.com>
2022-11-15 14:51:09 -0600

    11.15.22
    Fixed issues and cleaned up css

--- js/functions.js
+++ js/functions.js
@@ -24,7 +24,7 @@ function handleTabletChange(e) {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 $('html, body').animate({
-                scrollTop: target.offset().top-114
+                scrollTop: target.offset().top-142
                 }, 1000, function() {
                 // Callback after animation
                 // Must change focus!
